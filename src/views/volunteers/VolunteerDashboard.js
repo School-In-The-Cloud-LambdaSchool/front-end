ce0d85977b55bb24591ff80517aa9ac51caafa55
Matt Phillips <mattcphillips17@gmail.com>
2021-03-01 13:36:11 -0700

    finished functionality of register/login form

---
+++ src/views/volunteers/VolunteerDashboard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Volunteer(props) {
+
+
+
+  return (
+    <div>
+      <form>
+        <label>Username
+          <input
+            type='text'
+            name='username'
+            
+          />
+        </label>
+      </form>
+    </div>
+
+  )
+
+}
