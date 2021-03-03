b4f8e1feca63a6f7adc75d79dde1e2276d6ee86e
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-03 12:18:57 -0500

    removed comment out

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Login from './views/login/Login';
 import Register from './views/register/Register';
-// import AdminDashboard from './views/admin/AdminDashboard';
-// import AdminAddTask from './views/admin/AdminAddTask';
-// import AdminEditTask from './views/admin/AdminEditTask';
-// import VolunteerDashboard from './views/volunteers/VolunteerDashboard';
-// import StudentDashboard from './views/students/StudentDashboard';
-// import AdminStudents from './views/admin/AdminStudents';
+import AdminDashboard from './views/admin/AdminDashboard';
+import AdminAddTask from './views/admin/AdminAddTask';
+import AdminEditTask from './views/admin/AdminEditTask';
+import VolunteerDashboard from './views/volunteers/VolunteerDashboard';
+import StudentDashboard from './views/students/StudentDashboard';
+import AdminStudents from './views/admin/AdminStudents';
 
 function App() {
   return (
@@ -28,12 +28,12 @@ function App() {
           <Register />
         </Route>
 
-        {/* <PrivateRoute path='/admin/create-task' component={AdminAddTask} />
+        <PrivateRoute path='/admin/create-task' component={AdminAddTask} />
         <PrivateRoute path='/admin/edit-task' component={AdminEditTask} />
         <PrivateRoute path='/admin/students' component={AdminStudents} />
         <PrivateRoute path='/admin/' component={AdminDashboard} />
         <PrivateRoute path='/volunteer/:volunteerId' component={VolunteerDashboard} />
-        <PrivateRoute path='/student/:studentId' component={StudentDashboard} /> */}
+        <PrivateRoute path='/student/:studentId' component={StudentDashboard} />
 
         <Route exact path='/'>
           <Home />
