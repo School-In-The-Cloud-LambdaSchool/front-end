fbaa86c061873e4af04094da5bc215abe8aa6bc7
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-05 18:26:45 -0500

    got some styling over

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import logo from '../assets/logo.jpg';
-import '../css/Navbar.css';
+import './assets/css/main.css';
 
 const Navbar = () => {
     return (
